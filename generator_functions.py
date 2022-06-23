e5922718325fad89c5440545f9d326ebb8de896d
Shashank Goud <s.goud@cloudfmgroup.com>
2022-06-23 15:09:55 +0100

    added function generate_barchart in generator functions

--- generator_functions.py
+++ generator_functions.py
@@ -3,9 +3,12 @@ from pathlib import Path
 import pickle
 import os
 import matplotlib.pyplot as plt
+from datetime import date
 
-from .processing_functions import (statement_for_biggest_ooh, preprocessing_for_statement, statement_for_total_ooh, preprocessing_for_piechart)
+from .processing_functions import (statement_for_biggest_ooh, preprocessing_for_statement, statement_for_total_ooh, preprocessing_for_piechart,preprocessing_for_barchart)
 from .pie_chart import piechart_comparison_design
+from .energy_meter_with_benchmarking import energy_meter_with_benchmarking
+from .barchart_with_occupancy import (import_occupancy,generate_day_code,energy_and_occupancy_barchart_design)
 
 files_folder = os.path.join(os.getcwd(), 'files/')
 figures_folder = os.path.join(os.getcwd(), 'figures/')
@@ -39,3 +42,51 @@ def generate_piechart(df_meta_with_value, asset_group,
 
     piechart_comparison_design(df_for_piechart, ncol=1,loc='center right')
     plt.savefig(directory_to_savefig+"consumption_by_assetclass_piechart_mindsett.png",format='png', dpi=200)
+
+    consumption_mwh_cur = df_for_piechart['sum'].sum()/1000
+    consumption_mwh_pre = df_for_piechart['sum_pre'].sum()/1000
+
+    return consumption_mwh_cur, consumption_mwh_pre
+
+
+def generate_barchart_with_occupancy(db_occupancy, site_name, df_meta_with_value, 
+                                     month_current=None, 
+                                     occupancy_available = False,
+                                     tick_range_e=None,
+                                     tick_range_o=[-5, 40],
+                                     top_hours=True,
+                                     directory_to_savefig='./figures/'):
+
+    if month_current is None:
+        today = date.today()
+        month_current = int(today.strftime("%m")) - 1
+
+    df_meta_with_value_for_barchart = df_meta_with_value.loc[df_meta_with_value.month==month_current]
+
+    df_pivot_working_hours = preprocessing_for_barchart(df_meta_with_value_for_barchart)
+
+    if occupancy_available: 
+        # import occupancy data
+        df_occupancy_cur = import_occupancy(db_occupancy, site_name)
+    else:
+        df_occupancy_cur = None
+
+    day_code_list = generate_day_code(df_meta_with_value_for_barchart)
+
+    # barchart with occupancy
+
+    if tick_range_e is None:
+        
+        tick_range_e = df_pivot_working_hours.sum(axis=1).max()*1.4
+    
+    energy_and_occupancy_barchart_design(df_pivot_working_hours,
+                                             day_code_list,
+                                             df_occupancy_cur = df_occupancy_cur,
+                                             tick_range_e=tick_range_e,
+                                             tick_range_o=tick_range_o,
+                                             top_hours=top_hours)
+
+    
+    # Specify the directory to save figures, if it does not exist, create it
+    Path(directory_to_savefig).mkdir(parents=True, exist_ok=True)
+    plt.savefig(directory_to_savefig+"daily_consumption_barchart_with_occupancy_mar_with_pattern_MWh.png",format='png', dpi=200)
