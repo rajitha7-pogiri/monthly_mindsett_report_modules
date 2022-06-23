a0116e5dcecf8e1d588660a5234fd372b9f5426b
Shashank Goud <s.goud@cloudfmgroup.com>
2022-06-23 14:52:05 +0100

    added function generate_piechart

--- generator_functions.py
+++ generator_functions.py
@@ -1,10 +1,14 @@
+
 from pathlib import Path
 import pickle
 import os
+import matplotlib.pyplot as plt
 
-from .processing_functions import (statement_for_biggest_ooh, preprocessing_for_statement, statement_for_total_ooh)
+from .processing_functions import (statement_for_biggest_ooh, preprocessing_for_statement, statement_for_total_ooh, preprocessing_for_piechart)
+from .pie_chart import piechart_comparison_design
 
 files_folder = os.path.join(os.getcwd(), 'files/')
+figures_folder = os.path.join(os.getcwd(), 'figures/')
 
 def generate_insight_statements(df_meta_with_value, directory_to_savefile='./files/'): # todo: update the default value for directory
 
@@ -23,3 +27,15 @@ def generate_insight_statements(df_meta_with_value, directory_to_savefile='./fil
 
     with open(directory_to_savefile+'statements.pkl', 'wb') as f:
         pickle.dump(statements_list, f)
+
+
+def generate_piechart(df_meta_with_value, asset_group, 
+                      directory_to_savefig = './figures/'):
+
+    df_for_piechart = preprocessing_for_piechart(df_meta_with_value, asset_group=asset_group)
+
+    # Specify the directory to save figures, if it does not exist, create it
+    Path(directory_to_savefig).mkdir(parents=True, exist_ok=True)
+
+    piechart_comparison_design(df_for_piechart, ncol=1,loc='center right')
+    plt.savefig(directory_to_savefig+"consumption_by_assetclass_piechart_mindsett.png",format='png', dpi=200)
