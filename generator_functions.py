77d1f3ce362f28831320cb2ace4fad225776290d
localadmin <localadmin@RajithaPogiri.cloudfmgroup.com>
2022-07-05 17:45:14 +0100

    changes to energy meter on image formatting

--- generator_functions.py
+++ generator_functions.py
@@ -118,7 +118,7 @@ def generate_energy_meter_with_benchmarking(df_meta_with_value_building, floor_s
     energy_meter_with_benchmarking(consumption_mwh_cur, consumption_mwh_pre, floor_sqm, 
                                     industry=industry, period=period, size_in_sqm=size_in_sqm)
     Path(directory_to_savefig).mkdir(parents=True, exist_ok=True)
-    plt.savefig(directory_to_savefig+"Monthly_total_and_bm_latest.png",format='png', dpi=200)
+    plt.savefig(directory_to_savefig+"Monthly_total_and_bm_latest.png", format='png', dpi=200,transparent=True, bbox_inches='tight', pad_inches=0)
 
 
 
