dfc01e5612c521eb766ff8601f7b07867e35290d
localadmin <localadmin@RajithaPogiri.cloudfmgroup.com>
2022-07-07 16:59:02 +0100

    benchmarking and em  testing

--- energy_report/generate_energy_meter_with_benchmarking/__init__.py
+++ energy_report/generate_energy_meter_with_benchmarking/__init__.py
@@ -12,6 +12,8 @@ def generate_energy_meter_with_benchmarking(df_meta_with_value_building, floor_s
                                             
     consumption_mwh_cur, consumption_mwh_pre, days_in_period = preprocessing_for_energy_meter_with_benchmarking(df_meta_with_value_building)
 
+    print("days_in_period: ", days_in_period)
+
     energy_meter_with_benchmarking(consumption_mwh_cur, consumption_mwh_pre, floor_sqm, 
                                     industry=industry, period=days_in_period, size_in_sqm=size_in_sqm)
     Path(directory_to_savefig).mkdir(parents=True, exist_ok=True)
