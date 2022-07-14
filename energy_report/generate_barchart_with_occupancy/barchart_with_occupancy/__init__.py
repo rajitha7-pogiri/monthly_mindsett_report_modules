6f405ccef9d061c97add615fe22cd48d6f7546cc
localadmin <localadmin@RajithaPogiri.cloudfmgroup.com>
2022-07-14 12:46:05 +0100

    adjustment to occpancy

--- energy_report/generate_barchart_with_occupancy/barchart_with_occupancy/__init__.py
+++ energy_report/generate_barchart_with_occupancy/barchart_with_occupancy/__init__.py
@@ -23,6 +23,9 @@ def energy_and_occupancy_barchart_design(df_pivot_working_hours,
         if tick_range_e is None:
             tick_range_e = df_pivot_working_hours.sum(axis=1).max()*1.4
 
+        if tick_range_o is None:
+            tick_range_o = df_pivot_working_hours.sum(axis=1).max()*1.4
+
         white_padding_below_bar = tick_range_e/100
         white_padding_below_bar_for_legend = white_padding_below_bar/3
 
@@ -53,7 +56,7 @@ def energy_and_occupancy_barchart_design(df_pivot_working_hours,
                 # the right y axis
                 ax_r = ax_l.twinx() # instantiate a second axes that shares the same x-axis
                 ax_r.set_ylabel("People Registered", labelpad=10, fontsize ='12')
-                ax_r.set_ylim(tick_range_o)
+                ax_r.set_ylim([-5,tick_range_e])
                 ax_r.plot(df_occupancy_cur['occupancy'], color= 'k', lw=0.6, ls='dashed', marker=".", ms=6, mec="k", label='Occupancy')
                 ax_r.legend(loc='upper right', bbox_to_anchor=(0.97, 0.98))
 
