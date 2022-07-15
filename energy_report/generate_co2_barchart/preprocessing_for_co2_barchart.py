e77fcb8b2cd5a34f47255c92b667e4cbf2ea12e7
localadmin <localadmin@RajithaPogiri.cloudfmgroup.com>
2022-07-15 17:31:43 +0100

    changes to handle error

--- energy_report/generate_co2_barchart/preprocessing_for_co2_barchart.py
+++ energy_report/generate_co2_barchart/preprocessing_for_co2_barchart.py
@@ -7,8 +7,9 @@ def preprocessing_for_co2_barchart(df_meta_with_value,
     # df_meta_with_value[period_column]  = df_meta_with_value.index.tz_convert(None).to_period(freq)
 
     df_grouped_working_hours_period = df_meta_with_value.groupby([period_column, out_of_hours_column]).sum()[kwh_column]
-
-
+    
+    print('df_meta_with_value: ', df_meta_with_value)
+    print('df_grouped_working_hours_period: ', df_grouped_working_hours_period)
     df_grouped_working_hours_period_unstacked = df_grouped_working_hours_period.unstack()
     df_grouped_working_hours_period_unstacked = df_grouped_working_hours_period_unstacked.sort_index()
 
