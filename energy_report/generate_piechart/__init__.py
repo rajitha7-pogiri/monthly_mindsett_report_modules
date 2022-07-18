14645bf35ac0d3f4764ff657a0f2d763f309d9a7
localadmin <localadmin@RajithaPogiri.cloudfmgroup.com>
2022-07-18 11:58:16 +0100

    changes to handel image space padding piechart

--- energy_report/generate_piechart/__init__.py
+++ energy_report/generate_piechart/__init__.py
@@ -14,5 +14,5 @@ def generate_piechart(df_meta_with_value, asset_group,
     # Specify the directory to save figures, if it does not exist, create it
     Path(directory_to_savefig).mkdir(parents=True, exist_ok=True)
 
-    piechart_comparison_design(df_for_piechart, ncol=1,loc='center right', path_for_fig= directory_to_savefig+"consumption_by_assetclass_piechart_mindsett_internal.jpg")
-    plt.savefig(directory_to_savefig+"consumption_by_assetclass_piechart_mindsett.jpg",format='jpg', dpi=200)
+    piechart_comparison_design(df_for_piechart, ncol=1,loc='center right')
+    plt.savefig(directory_to_savefig+"consumption_by_assetclass_piechart_mindsett.png",format='png', dpi=200)
