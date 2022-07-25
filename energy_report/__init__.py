6af572dc645270a73fe86fa07f43629ec8e4bfcb
localadmin <localadmin@RajithaPogiri.cloudfmgroup.com>
2022-07-25 17:16:03 +0100

    added -- to piechart

--- energy_report/__init__.py
+++ energy_report/__init__.py
@@ -68,7 +68,7 @@ def energy_report(cf):
 
     generate_piechart(df_meta_with_value, cf.asset_group)
     
-    generate_energy_meter_with_benchmarking(df_meta_with_value_building, cf.floor_sqm, industry=cf.industry)
+    generate_energy_meter_with_benchmarking(df_meta_with_value_building, cf.floor_size, size_in_sqm=cf.size_in_sqm, industry=cf.industry)
 
     generate_barchart_with_occupancy(cf.postgresdb, cf.site_name, df_meta_with_value, occupancy_available=cf.occupancy_available)
 
--- energy_report/generate_energy_meter_with_benchmarking/__init__.py
+++ energy_report/generate_energy_meter_with_benchmarking/__init__.py
@@ -5,7 +5,7 @@ from .preprocessing_for_energy_meter_with_benchmarking import preprocessing_for_
 from .energy_meter_with_benchmarking import energy_meter_with_benchmarking
 
 
-def generate_energy_meter_with_benchmarking(df_meta_with_value_building, floor_sqm,
+def generate_energy_meter_with_benchmarking(df_meta_with_value_building, floor_size,
                                             industry = "office",
                                             size_in_sqm = True,
                                             directory_to_savefig='./figures/'):
@@ -13,7 +13,7 @@ def generate_energy_meter_with_benchmarking(df_meta_with_value_building, floor_s
     consumption_mwh_cur, consumption_mwh_pre, days_in_period = preprocessing_for_energy_meter_with_benchmarking(df_meta_with_value_building)
 
 
-    energy_meter_with_benchmarking(consumption_mwh_cur, consumption_mwh_pre, floor_sqm, 
+    energy_meter_with_benchmarking(consumption_mwh_cur, consumption_mwh_pre, floor_size, 
                                     industry=industry, period=days_in_period, size_in_sqm=size_in_sqm)
     Path(directory_to_savefig).mkdir(parents=True, exist_ok=True)
     plt.savefig(directory_to_savefig+"Monthly_total_and_bm_latest.png", format='png', dpi=200,transparent=True, bbox_inches='tight', pad_inches=0)
