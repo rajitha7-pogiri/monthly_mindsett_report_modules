e33e43c2e0e8addf528c648d350c5d5443e341b9
localadmin <localadmin@RajithaPogiri.cloudfmgroup.com>
2022-07-25 13:13:52 +0100

    added -- to piechart

--- energy_report/generate_co2_barchart/__init__.py
+++ energy_report/generate_co2_barchart/__init__.py
@@ -11,4 +11,6 @@ def generate_co2_barchart(df_meta_with_value_building,
 
     co2_barchart_design(df_grouped_working_hours_period_unstacked)
     Path(directory_to_savefig).mkdir(parents=True, exist_ok=True)
-    plt.savefig(directory_to_savefig+"Total_consumption_barchart_with_Co2.png",format='png', dpi=200, bbox_inches='tight', pad_inches=0)
+
+    # todo: to be added - transparent=True, bbox_inches='tight', pad_inches=0 
+    plt.savefig(directory_to_savefig+"Total_consumption_barchart_with_Co2.png",format='png', dpi=200) 
